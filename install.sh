57312603cd6ce4479485c2b1e71048313773c134
Govind Pimpale <gpimpale29@gmail.com>
2018-04-11 16:02:29 -0700

    fixed my installer

--- install.sh
+++ install.sh
@@ -2,11 +2,20 @@
 
 pwd=$(pwd)
 
-ln -si "$pwd/zshrc" ~/.zshrc 
-ln -si "$pwd/ycm_extra_conf.py" ~/.ycm_extra_conf.py
-ln -si "$pwd/nvim" ~/.config
-ln -si "$pwd/htop" ~/.config
-ln -si "$pwd/i3" ~/.config
-ln -si "$pwd/i3status" ~/.config
-ln -si "$pwd/zathura" ~/.config
+
+rm -ri ~/.zshrc
+rm -ri ~/.ycm_extra_conf.py
+rm -ri ~/.config/nvim
+rm -ri ~/.config/htop
+rm -ri ~/.config/i3
+rm -ri ~/.config/i3status
+rm -ri ~/.config/zathura
+
+ln -s "$pwd/zshrc" ~/.zshrc 
+ln -s "$pwd/ycm_extra_conf.py" ~/.ycm_extra_conf.py
+ln -s "$pwd/nvim" ~/.config
+ln -s "$pwd/htop" ~/.config
+ln -s "$pwd/i3" ~/.config
+ln -s "$pwd/i3status" ~/.config
+ln -s "$pwd/zathura" ~/.config
 

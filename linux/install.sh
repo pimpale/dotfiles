e329870f43234d18e0140db66bd1fba1b9445481
Govind <gpimpale29@gmail.com>
2018-05-10 15:23:40 -0700

    big changes

--- linux/install.sh
+++ linux/install.sh
@@ -10,6 +10,9 @@ rm -ri ~/.config/i3
 rm -ri ~/.config/i3status
 rm -ri ~/.config/zathura
 
+mkdir ~/.config
+mkdir ~/.emacs.d
+
 ln -s "$pwd/zshrc" ~/.zshrc 
 ln -s "$pwd/ycm_extra_conf.py" ~/.ycm_extra_conf.py
 ln -s "$pwd/nvim" ~/.config
@@ -17,4 +20,3 @@ ln -s "$pwd/htop" ~/.config
 ln -s "$pwd/i3" ~/.config
 ln -s "$pwd/i3status" ~/.config
 ln -s "$pwd/zathura" ~/.config
-

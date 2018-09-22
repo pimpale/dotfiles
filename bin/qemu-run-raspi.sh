d22d59182515d7fbb1480b4860586dc38f259ac2
pimpale <gpimpale29@gmail.com>
2018-09-21 18:22:02 -0700

    renamed stuff

--- bin/qemu-run-raspi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qemu-system-arm -kernel ~/qemu_vms/<your-kernel-qemu> -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/ rootfstype=ext4 rw" -hda ~/qemu_vms/<your-jessie-image.img> -redir tcp:5022::22 -no-reboot

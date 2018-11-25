edd9bdd87f2c00d72bff57c6dbf9d096714300f6
pimpale <gpimpale29@gmail.com>
2018-11-25 07:41:32 -0800

    added a hacky force option to installer script (also dmenu is more glorious)

--- install.zsh
+++ install.zsh
@@ -1,11 +1,14 @@
 #!/bin/zsh
 
+opts=$@
+
 pwd="$HOME/dotfiles"
 mkdir "$HOME/.config"
 
-ln -s "$pwd/zshrc" "$HOME/.zshrc"
-ln -s "$pwd/nvim" "$HOME/.config"
-ln -s "$pwd/htop" "$HOME/.config"
-ln -s "$pwd/i3" "$HOME/.config"
-ln -s "$pwd/i3status" "$HOME/.config"
-ln -s "$pwd/zathura" "$HOME/.config"
+ln -s $opts "$pwd/zshrc" "$HOME/.zshrc"
+ln -s $opts "$pwd/nvim" "$HOME/.config"
+ln -s $opts "$pwd/htop" "$HOME/.config"
+ln -s $opts "$pwd/i3" "$HOME/.config"
+ln -s $opts "$pwd/i3status" "$HOME/.config"
+ln -s $opts "$pwd/zathura" "$HOME/.config"
+ln -s $opts "$pwd/rofi" "$HOME/.config"

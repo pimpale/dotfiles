6efda2a43ba9f67b604d12536de9a348346f149d
gpi <gpimpale29@gmail.com>
2019-06-03 07:09:27 -0700

    updated for smaller screen computer

--- install.zsh
+++ install.zsh
@@ -6,7 +6,6 @@ pwd="$HOME/dotfiles"
 mkdir "$HOME/.config"
 
 ln -s $opts "$pwd/zshrc" "$HOME/.zshrc"
-ln -s $opts "$pwd/xonshrc" "$HOME/.xonshrc"
 ln -s $opts "$pwd/nvim" "$HOME/.config"
 ln -s $opts "$pwd/i3" "$HOME/.config"
 ln -s $opts "$pwd/i3status" "$HOME/.config"

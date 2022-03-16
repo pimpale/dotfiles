c00082d597e1e409940b163a041748e7655e20a3
gpi <gpimpale29@gmail.com>
2022-03-15 18:45:37 -0700

    nice

--- fish/config.fish
+++ fish/config.fish
@@ -23,3 +23,9 @@ function e
 end
 
 fzf_key_bindings
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+

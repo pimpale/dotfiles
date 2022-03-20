b1befc4f79322c233cdf838b6b6318764f1c5d8b
gpi <gpimpale29@gmail.com>
2022-03-20 10:16:55 -0700

    stuff

--- bin/fixtablet.sh
+++ bin/fixtablet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Change DVI-I-1 to what monitor you want from running command: xrandr
-MONITOR="HDMI-0"
+MONITOR="DP-1"
 ID_STYLUS=`xinput | grep "Pen Pen" | cut -f 2 | cut -c 4-5`
 
 xinput map-to-output $ID_STYLUS $MONITOR

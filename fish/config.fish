dbc79c354705d2327c2376d0e9aab2e6365c724c
gpi <gpimpale29@gmail.com>
2022-03-27 21:26:02 -0700

    export vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,20 @@
-set PATH $HOME/bin $PATH
-set PATH $HOME/.local/bin $PATH
-set PATH $HOME/.cargo/bin $PATH
-set PATH $HOME/.yarn/bin $PATH
+set -x PATH $HOME/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/.yarn/bin $PATH
 
-set PATH $HOME/dotfiles/bin $PATH
+set -x PATH $HOME/dotfiles/bin $PATH
 
-set ANDROID_HOME $HOME/Android/Sdk
+set -x ANDROID_HOME $HOME/Android/Sdk
 
-set PATH $ANDROID_HOME/emulator $PATH
-set PATH $ANDROID_HOME/tools $PATH
-set PATH $ANDROID_HOME/tools/bin $PATH
-set PATH $ANDROID_HOME/platform-tools $PATH
+set -x PATH $ANDROID_HOME/emulator $PATH
+set -x PATH $ANDROID_HOME/tools $PATH
+set -x PATH $ANDROID_HOME/tools/bin $PATH
+set -x PATH $ANDROID_HOME/platform-tools $PATH
 
-set VISUAL "kak"
-set EDITOR "kak"
-set LC_CTYPE "en_US.UTF-8"
+set -x VISUAL "kak"
+set -x EDITOR "kak"
+set -x LC_CTYPE "en_US.UTF-8"
 
 set fish_greeting ""
 
